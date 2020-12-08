9a1d3bf21adeeb4629ceb078e7310f012edf3028
Kotova9478 <Kotova9478@gmail.com>
2020-12-08 09:22:15 -0800

    add LisaJ_test

--- src/test/java/GroupSfbaGrizzlies.java
+++ src/test/java/GroupSfbaGrizzlies.java
@@ -9,12 +9,59 @@ public class GroupSfbaGrizzlies extends BaseTest {
 
     @Test
     public void klavdiaGoldshteyn() throws InterruptedException {
-       WebDriver browser = getDriver();
-       browser.get("https://en.wikipedia.org/wiki/Roger_Federer");
-       WebElement title = browser.findElement(By.xpath("//h1[@id='firstHeading']"));
+        WebDriver browser = getDriver();
+        browser.get("https://en.wikipedia.org/wiki/Roger_Federer");
+        WebElement title = browser.findElement(By.xpath("//h1[@id='firstHeading']"));
 
-       Assert.assertEquals(title.getText(), "Roger Federer");
+        Assert.assertEquals(title.getText(), "Roger Federer");
 
-       Thread.sleep(3000);
-   }
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void lisaJohns() throws InterruptedException {
+        WebDriver browser = getDriver();
+        browser.get("https://www.rideboreal.com/");
+
+        WebElement title = browser.findElement(By.xpath("//div/h1[text()='HOME AT BOREAL']"));
+
+        Assert.assertEquals(title.getText(), "HOME AT BOREAL");
+        Thread.sleep(3000);
+
+        browser.findElement(By.xpath("//div[@class='alert-content animated fadeInUp']"));
+        Thread.sleep(3000);
+
+        browser.findElement(By.xpath("//nav[contains(@img, ng-scope)]"));
+        Thread.sleep(3000);
+
+        browser.findElement(By.xpath("//a[@class='u-txt-uppercase ng-scope btn']")).click();
+        Thread.sleep(3000);
+
+        browser.findElement(By.xpath("//a[@class='u-txt-uppercase campwoodward-link ng-scope btn']")).click();
+        Thread.sleep(3000);
+
+        browser.findElement(By.xpath("//button[@class='feed']")).click();
+        Thread.sleep(3000);
+
+        browser.findElement(By.xpath("//a[@class='register']")).click();
+        Thread.sleep(3000);
+
+        browser.findElement(By.xpath("//a[text()='Log In']")).click();
+        Thread.sleep(3000);
+
+        browser.findElement(By.xpath("//a[@class='promotion-button valign']"));
+        Thread.sleep(3000);
+
+       // browser.findElement(By.xpath("//button/a[text()='Buy Lift Tickets']"));
+       // Thread.sleep(3000);
+
+
+
+
+
+
+
+
+
+    }
 }
